9d8d2fe187418ee9448ea52a60a0f458f30f66bd
zacbayhan <zacbayhan@gmail.com>
2017-09-07 22:01:20 -0400

    updated project

--- main.go
+++ main.go
@@ -12,10 +12,11 @@ const (
   port = ":8080"
   elkPort = ":9200"
   elkStack = "localhost"
+  action = "_search"
 )
 
 var calls = 0
-
+/*
 type Token struct {
     Raw       string                 // The raw token.  Populated when you Parse a token
     Method    SigningMethod          // The signing method used or to be used
@@ -28,12 +29,10 @@ type Token struct {
 func handler(w http.ResponseWriter, r *http.Request) {
     return
 }
-
+*/
 
 func elk_query(w http.ResponseWriter, r *http.Request) {
   calls++
-  r.HandleFunc("/{index}", search).Host(elkStack:elkPort).Methods("GET", "POST").Schemes("http")
-
   fmt.Fprintf(w, "Hello, world! You have called me %d times.\n", calls)
 }
 
@@ -42,21 +41,34 @@ func init() {
   http.HandleFunc("/", elk_query)
   http.ListenAndServe(port, nil)
 }
-
+/*
 func auth() {
-tokenString := "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJmb28iOiJiYXIiLCJuYmYiOjE0NDQ0Nzg0MDB9.u1riaD1rW97opCoAuRCTy4w58Br-Zk-bh7vLiRIsrpU"
-token, err := jwt.Parse(tokenString, func(token *jwt.Token) (interface{}, error) {
-    if _, ok := token.Method.(*jwt.SigningMethodHMAC); !ok {
-        return nil, fmt.Errorf("Unexpected signing method: %v", token.Header["alg"])
-    }
-    return hmacSampleSecret, nil
-})
-if claims, ok := token.Claims.(jwt.MapClaims); ok && token.Valid {
-    fmt.Println(claims["foo"], claims["nbf"])
-} else {
-    fmt.Println(err)
-  }
+    tokenString := "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJmb28iOiJiYXIiLCJuYmYiOjE0NDQ0Nzg0MDB9.u1riaD1rW97opCoAuRCTy4w58Br-Zk-bh7vLiRIsrpU"
+    token, err := jwt.Parse(tokenString, func(token *jwt.Token) (interface{}, error) {
+        if _, ok := token.Method.(*jwt.SigningMethodHMAC); !ok {
+            return nil, fmt.Errorf("Unexpected signing method: %v", token.Header["alg"])
+        }
+        return hmacSampleSecret, nil
+    })
+    if claims, ok := token.Claims.(jwt.MapClaims); ok && token.Valid {
+        //add elk_query here
+        elk_query
+        fmt.Println(claims["foo"], claims["nbf"])
+    } else {
+        fmt.Println(err)
+      }
 }
-
+*/
 
 func main() {}
+
+/*
+{
+  r := mux.NewRouter()
+  // Routes consist of a path and a handler function.
+  r.HandleFunc("/", YourHandler)
+
+  // Bind to a port and pass our router in
+  log.Fatal(http.ListenAndServe(port, r))
+}
+*/
